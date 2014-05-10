4da611bd42fca23f90aa3f46547c9e9da548fe5e
seyfer <seyferseed@mail.ru>
2014-05-10 10:05:25 +0800

    From windows

---
+++ src/com/javarush/test/level04/lesson06/task01/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level04.lesson06.task01;
+
+/* Минимум двух чисел
+Ввести с клавиатуры два числа, и вывести на экран минимальное из них.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //Напишите тут ваш код
+
+    }
+}
