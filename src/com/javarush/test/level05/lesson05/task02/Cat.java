8aef5034addd81325327272395c683105568bd14
seyfer <seyferseed@mail.ru>
2015-12-23 23:07:54 +0700

    start level 5 tasks

---
+++ src/com/javarush/test/level05/lesson05/task02/Cat.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level05.lesson05.task02;
+
+/* Реализовать метод fight
+Реализовать метод boolean fight(Cat anotherCat):
+реализовать механизм драки котов в зависимости от их веса, возраста и силы.
+Зависимость придумать самому. Метод должен определять, выиграли ли мы (this) бой или нет,
+т.е. возвращать true, если выиграли и false - если нет.
+Должно выполняться условие:
+если cat1.fight(cat2) = true , то cat2.fight(cat1) = false
+*/
+
+public class Cat
+{
+    public String name;
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat()
+    {
+    }
+
+    public boolean fight(Cat anotherCat)
+    {
+        //напишите тут ваш код
+        int points = 0;
+        int anotherCatPoints = 0;
+
+        points = formula();
+        anotherCatPoints = anotherCat.formula();
+
+        return points >= anotherCatPoints;
+    }
+
+    private int formula() {
+        return  weight + strength + (int) Math.ceil(age/2);
+    }
+}
