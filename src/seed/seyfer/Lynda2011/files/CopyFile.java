dd2240387e22e9eba1b8821283d3075d6c9760ee
seyfer <seyferseed@mail.ru>
2015-12-23 22:55:19 +0700

    fix config

--- src/seed/seyfer/Lynda2011/files/CopyFile.java
+++ src/seed/seyfer/Lynda2011/files/CopyFile.java
@@ -1,15 +1,10 @@
 package seed.seyfer.Lynda2011.files;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.commons.io.FileUtils;
 
 /**
  *
