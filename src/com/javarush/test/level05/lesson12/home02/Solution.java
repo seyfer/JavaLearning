c2ba59a8607e710491afdb1880668035052bffc6
seyfer <seyferseed@mail.ru>
2015-12-25 13:58:48 +0700

    fix

---
+++ src/com/javarush/test/level05/lesson12/home02/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level05.lesson12.home02;
+
+/* Man and Woman
+1. Внутри класса Solution создай public static классы Man и Woman.
+2. У классов должны быть поля: name(String), age(int), address(String).
+3. Создай конструкторы, в которые передаются все возможные параметры.
+4. Создай по два объекта каждого класса со всеми данными используя конструктор.
+5. Объекты выведи на экран в таком формате [name + " " + age + " " + address].
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //создай по два объекта каждого класса тут
+
+        //выведи их на экран тут
+    }
+
+    //добавьте тут ваши классы
+}
