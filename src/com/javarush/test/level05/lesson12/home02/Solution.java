283b4987674a9eb731e9dd80e6a509c0ac6bde87
seyfer <seyferseed@mail.ru>
2015-12-26 21:09:46 +0700

    additional tasks

--- src/com/javarush/test/level05/lesson12/home02/Solution.java
+++ src/com/javarush/test/level05/lesson12/home02/Solution.java
@@ -13,9 +13,42 @@ public class Solution
     public static void main(String[] args)
     {
         //создай по два объекта каждого класса тут
+        Man man1 = new Man("Mikhail", 25, "hz");
+        Man man2 = new Man("Oleg", 27, "hz2");
+
+        Woman woman1 = new Woman("Shluha", 22, "podval");
+        Woman woman2 = new Woman("Shluha", 22, "podval");
 
         //выведи их на экран тут
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+
+        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
     }
 
     //добавьте тут ваши классы
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
 }
---
+++ src/com/javarush/test/level05/lesson12/home05/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level05.lesson12.home05;
+
+/* Вводить с клавиатуры числа и считать их сумму
+Вводить с клавиатуры числа и считать их сумму, пока пользователь не введёт слово «сумма». Вывести на экран полученную сумму.
+*/
+
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        Scanner s = new Scanner(System.in);
+
+        String input = "";
+        int sum = 0;
+
+        while (!input.equals("сумма")) {
+            input = s.nextLine();
+
+            try {
+                sum += Integer.parseInt(input);
+            } catch (Exception e) {
+
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
