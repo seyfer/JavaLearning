89aacc3cc021db2198df18a2e6e58abcbde9c7c5
seyfer <seyferseed@mail.ru>
2015-12-28 00:26:17 +0700

    level 6 bonus

---
+++ src/com/javarush/test/level06/lesson11/bonus02/Solution.java
@@ -0,0 +1,116 @@
+package com.javarush.test.level06.lesson11.bonus02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* Нужно добавить в программу новую функциональность
+Задача: У каждой кошки есть имя и кошка-мама. Создать класс, который бы описывал данную ситуацию. Создать два объекта: кошку-дочь и кошку-маму. Вывести их на экран.
+Новая задача: У каждой кошки есть имя, кошка-папа и кошка-мама. Изменить класс Cat так, чтобы он мог описать данную ситуацию.
+Создать 6 объектов: дедушку(папин папа), бабушку(мамина мама), папу, маму, сына, дочь.
+Вывести их всех на экран в порядке: дедушка, бабушка, папа, мама, сын, дочь.
+
+Пример ввода:
+дедушка Вася
+бабушка Мурка
+папа Котофей
+мама Василиса
+сын Мурчик
+дочь Пушинка
+
+Пример вывода:
+Cat name is дедушка Вася, no mother, no father
+Cat name is бабушка Мурка, no mother, no father
+Cat name is папа Котофей, no mother, father is дедушка Вася
+Cat name is мама Василиса, mother is бабушка Мурка, no father
+Cat name is сын Мурчик, mother is мама Василиса, father is папа Котофей
+Cat name is дочь Пушинка, mother is мама Василиса, father is папа Котофей
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException {
+//        oldSolution();
+
+        newSolution();
+    }
+
+    private static void newSolution() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String grandFatherName = reader.readLine();
+        String grandMotherName = reader.readLine();
+        String fatherName = reader.readLine();
+        String motherName = reader.readLine();
+        String sonName = reader.readLine();
+        String daughterName = reader.readLine();
+
+        Cat grandFather = new Cat(grandFatherName);
+        Cat grandMother = new Cat(grandMotherName);
+        Cat father = new Cat(fatherName, grandFather, null);
+        Cat mother = new Cat(motherName, null, grandMother);
+        Cat son = new Cat(sonName, father, mother);
+        Cat daughter = new Cat(daughterName, father, mother);
+
+        System.out.println(grandFather);
+        System.out.println(grandMother);
+        System.out.println(father);
+        System.out.println(mother);
+        System.out.println(son);
+        System.out.println(daughter);
+    }
+
+    private static void oldSolution() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String motherName = reader.readLine();
+        Cat catMother = new Cat(motherName);
+
+        String daughterName = reader.readLine();
+        Cat catDaughter = new Cat(daughterName, catMother);
+
+        System.out.println(catMother);
+        System.out.println(catDaughter);
+    }
+
+    public static class Cat
+    {
+        private String name;
+        private Cat parent;
+        private Cat father;
+        private Cat mother;
+
+        Cat(String name) {
+            this.name = name;
+        }
+
+        Cat(String name, Cat parent) {
+            this.name = name;
+            this.parent = parent;
+        }
+
+        Cat(String name, Cat father, Cat mother) {
+            this.name = name;
+            this.father = father;
+            this.mother = mother;
+        }
+
+        public String oldToString() {
+            if (parent == null) {
+                return "Cat name is " + name + ", no mother ";
+            } else {
+                return "Cat name is " + name + ", mother is " + parent.name;
+            }
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder output = new StringBuilder("Cat name is " + name);
+
+            output.append(mother == null ? ", no mother" : ", mother is " + mother.name);
+            output.append(father == null ? ", no father" : ", father is " + father.name);
+
+            return output.toString();
+        }
+    }
+}
---
+++ src/com/javarush/test/level06/lesson11/bonus03/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.test.level06.lesson11.bonus03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/* Задача по алгоритмам
+Задача: Написать программу, которая вводит с клавиатуры 5 чисел и выводит их в возрастающем порядке.
+Пример ввода:
+3
+2
+15
+6
+17
+Пример вывода:
+2
+3
+6
+15
+17
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //напишите тут ваш код
+        int[] nums = new int[5];
+        int count = 5;
+
+        for (int i = 0; i < count; i++) {
+            nums[i] = Integer.parseInt(reader.readLine());
+        }
+
+        //debug
+//        System.out.println(Arrays.toString(nums));
+
+        nums = BubbleSort(nums);
+
+//        System.out.println(Arrays.toString(nums));
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    public static int[] BubbleSort(int[] num) {
+        int j;
+        boolean flag = true;   // set flag to true to begin first pass
+        int temp;   //holding variable
+
+        while (flag) {
+            flag = false;    //set flag to false awaiting a possible swap
+            for (j = 0; j < num.length - 1; j++) {
+                if (num[j] > num[j + 1])   // change to > for ascending sort
+                {
+                    temp = num[j];                //swap elements
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                    flag = true;              //shows a swap occurred
+                }
+            }
+        }
+
+        return num;
+    }
+}
