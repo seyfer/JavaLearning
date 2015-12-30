eff0f9679b67b11b81c940ed2aeefd5cf28039ec
seyfer <seyferseed@mail.ru>
2015-12-30 18:54:21 +0700

    level 7 bonus

---
+++ src/com/javarush/test/level07/lesson12/bonus03/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.test.level07.lesson12.bonus03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* Задача по алгоритмам
+Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++)
+        {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array)
+        {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array)
+    {
+        array = BubbleSort(array);
+    }
+
+    public static int[] BubbleSort(int[] num) {
+        int j;
+        boolean flag = true;   // set flag to true to begin first pass
+        int temp;   //holding variable
+
+        while (flag) {
+            flag = false;    //set flag to false awaiting a possible swap
+            for (j = 0; j < num.length - 1; j++) {
+                if (num[j] < num[j + 1])   // change to > for ascending sort
+                {
+                    temp = num[j];                //swap elements
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                    flag = true;              //shows a swap occurred
+                }
+            }
+        }
+
+        return num;
+    }
+}
