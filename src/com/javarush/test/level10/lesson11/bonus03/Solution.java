c63ed461c370ce523c9df01d14cf9d80cfcdf89d
seyfer <seyferseed@mail.ru>
2016-01-09 21:24:11 +0700

    complete bonus

--- src/com/javarush/test/level10/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level10/lesson11/bonus03/Solution.java
@@ -36,5 +36,27 @@ public class Solution
     public static void sort(int[] array)
     {
         //напишите тут ваш код
+        array = BubbleSort(array);
+    }
+
+    public static int[] BubbleSort(int[] num) {
+        int j;
+        boolean flag = true;   // set flag to true to begin first pass
+        int temp;   //holding variable
+
+        while (flag) {
+            flag = false;    //set flag to false awaiting a possible swap
+            for (j = 0; j < num.length - 1; j++) {
+                if (num[j] > num[j + 1])   // change to > for ascending sort
+                {
+                    temp = num[j];                //swap elements
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                    flag = true;              //shows a swap occurred
+                }
+            }
+        }
+
+        return num;
     }
 }
