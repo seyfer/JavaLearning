6f1d3055e116dbcd6fc5422a2e5565bdd29a0dff
seyfer <seyferseed@mail.ru>
2016-02-10 23:26:56 +0700

    level16 lesson7

---
+++ src/com/javarush/test/level16/lesson07/task03/Solution.java
@@ -0,0 +1,78 @@
+package com.javarush.test.level16.lesson07.task03;
+
+/* Big Ben clock
+1. Разберись, что делает программа.
+2. Реализуй логику метода printTime так, чтобы каждую секунду выдавалось время начиная с установленного в конструкторе
+Пример:
+В г. Лондон сейчас 23:59:58!
+В г. Лондон сейчас 23:59:59!
+В г. Лондон сейчас полночь!
+В г. Лондон сейчас 0:0:1!
+*/
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Solution {
+    public static volatile boolean isStopped = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Clock clock = new Clock("Лондон", 23, 59, 57);
+        Thread.sleep(4000);
+        isStopped = true;
+        Thread.sleep(1000);
+    }
+
+    public static class Clock extends Thread {
+        private String cityName;
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        public Clock(String cityName, int hours, int minutes, int seconds) {
+            this.cityName = cityName;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            start();
+        }
+
+        public void run() {
+            try {
+                while (!isStopped) {
+                    printTime();
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        private void printTime() throws InterruptedException {
+            //add your code here - добавь код тут
+
+            Date date = new Date();
+
+            // gets a calendar using the default time zone and locale.
+            Calendar calendar = Calendar.getInstance();
+            date.setHours(hours);
+            date.setMinutes(minutes);
+            date.setSeconds(seconds);
+            calendar.setTime(date);
+
+            //wait 1 second
+            Thread.sleep(1000);
+            calendar.add(Calendar.SECOND, 1);
+
+            //get new time
+            Date dateAfter = calendar.getTime();
+            hours = dateAfter.getHours();
+            minutes = dateAfter.getMinutes();
+            seconds = dateAfter.getSeconds();
+
+            if (hours == 0 && minutes == 0 && seconds == 0) {
+                System.out.println(String.format("В г. %s сейчас полночь!", cityName));
+            } else {
+                System.out.println(String.format("В г. %s сейчас %d:%d:%d!", cityName, hours, minutes, seconds));
+            }
+        }
+    }
+}
